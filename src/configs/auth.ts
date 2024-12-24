017a83099991e3dc9ef6dde71a8457cb6d85181b
styven <styvenn16@gmail.com>
2024-12-24 11:14:25 -0300

    Validando Variável de Ambiente

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,9 @@
+import { env } from "@/env"
+
+
 export const authConfig = {
     jwt: {
-        secret: process.env.JWT_SECRET,
+        secret: env.JWT_SECRET,
         expiresIn: "1d",
     }
 }

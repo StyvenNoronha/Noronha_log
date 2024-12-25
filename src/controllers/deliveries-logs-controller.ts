0c694cb4d1c070ab9a8833cd6910460a210d38f6
styven <styvenn16@gmail.com>
2024-12-25 11:20:45 -0300

    Cadastra Log

--- src/controllers/deliveries-logs-controller.ts
+++ src/controllers/deliveries-logs-controller.ts
@@ -1,7 +1,42 @@
 import {Request,Response} from 'express'
+import z from 'zod'
+import {prisma} from "@/database/prisma "
+import { AppError } from '@/utils/AppError'
 
 export class DeliveriesLogsController{
     async create(request:Request, response:Response){
-        return response.json({message:"ok"})
+        const bodySchema = z.object({
+            delivery_id: z.string().uuid(),
+            description: z.string()
+        })
+
+        const {delivery_id, description} = bodySchema.parse(request.body)
+
+
+       
+
+        const delivery = await prisma.delivery.findUnique({
+            where:{id: delivery_id}
+        })
+
+        if(!delivery){
+            throw new AppError("entrega não encontrada")
+        }
+
+        if(delivery.status === "processing"){
+            throw new AppError("alterar o status para enviado")
+        }
+
+
+        await prisma.deliveryLog.create({
+            data:{
+                deliveryId: delivery_id,
+                description
+            }
+        })
+
+
+
+        return response.status(201).json()
     }
 }

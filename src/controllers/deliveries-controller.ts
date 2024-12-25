9d0614be1e9115e0ef3a1b2ec4612b5600ff84ca
styven <styvenn16@gmail.com>
2024-12-25 10:09:34 -0300

    cadastrando entrega

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,9 +1,22 @@
-import {Request,Response,NextFunction} from 'express'
-
-
+import {Request,Response} from 'express'
+import {prisma} from "@/database/prisma "
+import z from "zod"
 class DeliveriesController{
-    create(request:Request, response:Response){
-        return response.json({message:"ok"})
+    async create(request:Request, response:Response){
+        const bodySchema = z.object({
+            user_id:z.string().uuid(),
+            description: z.string()
+        })
+
+        const {user_id, description} = bodySchema.parse(request.body)
+
+        await prisma.delivery.create({
+            data:{
+                userId: user_id,
+                description
+            }
+        })
+        return response.status(201).json({message:"ok"})
     }
 }
 

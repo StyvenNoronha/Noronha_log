33a966354c099dea9ca08f509f3fd79e4a8c7da7
styven <styvenn16@gmail.com>
2024-12-25 10:47:30 -0300

    atualizando status de entrega

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,16 +1,18 @@
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
-
+import {DeliveriesStatusController} from"@/controllers/deliveries-status-controller"
 import { Router } from "express";
 
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 
 deliveriesRoutes.use(ensureAuthenticated,verifyUserAuthorization(["sale"]))
 deliveriesRoutes.post("/", deliveriesController.create)
 deliveriesRoutes.get("/", deliveriesController.index)
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update)
 
 export { deliveriesRoutes}

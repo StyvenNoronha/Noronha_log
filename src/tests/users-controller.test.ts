9aacf5dccfb2aba1d1e5424ef19db4d4396e0735
styven <styvenn16@gmail.com>
2024-12-25 15:40:21 -0300

    Teste no Cadastro do Usuário

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,3 +1,15 @@
+import request from "supertest"
+import {app} from "@/app"
+
 describe("UsersController",()=>{
-    console.log("Passou por aqui")
+    it("should create a new user successfully",async ()=>{
+        const response = await request(app).post("/users").send({
+            name:"rodrigo",
+            email:"rodrigoo@example.com",
+            password:"123456seven"
+        })
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty("id")
+        expect(response.body.name).toBe("rodrigo")
+    })
 })
